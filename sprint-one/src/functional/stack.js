988322c65aba84e1bc1451e75eda6766d21d2d70
melindab <melindab@users.noreply.github.com>
2015-08-20 13:49:41 -0500

    Implement functional queue and stack.

---
+++ sprint-one/src/functional/stack.js
@@ -0,0 +1,28 @@
+var Stack = function(){
+  var someInstance = {};
+  var count = 0;
+
+  // Use an object with numeric keys to store values
+  var storage = {}; 
+
+  // Implement the methods below
+  someInstance.push = function(value){
+    someInstance[count] = value;
+    count++;
+  };
+
+  someInstance.pop = function(){
+    if (count) {
+      count--;
+    }
+    var popped = someInstance[count];
+    delete someInstance[count];
+    return popped;
+  };
+
+  someInstance.size = function(){
+    return count;
+  };
+
+  return someInstance;
+};
